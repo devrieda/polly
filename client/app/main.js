efadd11cbcc734b60c79128f9d9185babf3fbc86
Jason Madsen <jmadsen@instructure.com>
2014-06-24 15:04:02 -0600

    clean up react dev run, print react to rails app

--- client/app/main.js
+++ client/app/main.js
@@ -3,7 +3,7 @@
 var Router = require('react-nested-router').Router
 var env = require('../config/env')
 
-require('./router').renderComponent(document.body)
+require('./router').renderComponent(document.querySelector('#polly'));
 
 // TODO: figure out what module.exports was used for -- functional tests?
 //module.exports = app
--- client/app/views/index.js
+++ client/app/views/index.js
@@ -8,7 +8,7 @@ module.exports = React.createClass({
   render: function() {
     return (
       <div>
-        Polly!
+        Polly React App!
       </div>
     )
   }
