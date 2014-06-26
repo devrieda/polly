7a64663d9b54eb2ca814a06c621af2c164989d9a
Derek DeVries <devrieda@gmail.com>
2014-06-26 11:52:08 -0600

    complete styles for sidebar navigation

--- client/app/views/drawer_nav.js
+++ client/app/views/drawer_nav.js
@@ -12,19 +12,58 @@ module.exports = React.createClass({
 
         <h3>Open Polls</h3>
         <ul className="nav-polls open">
-          {this.props.polls}
+          <li className="available">
+            <Link to="poll" id="1">
+              <span className="question">
+                Who is your favorite Arrested Development character?
+              </span>
+              <span className="date">
+                Today
+              </span>
+            </Link>
+          </li>
+          <li className="">
+            <Link to="poll" id="2">
+              <span className="question">
+                What is your favorite color?
+              </span>
+              <span className="date">
+                6/11/2014
+              </span>
+            </Link>
+          </li>
+          <li className="">
+            <Link to="poll" id="3">
+              <span className="question">
+                What soccer team is going to win the World Cup?
+              </span>
+              <span className="date">
+                6/10/2014
+              </span>
+            </Link>
+          </li>
         </ul>
 
         <h3>Closed Polls</h3>
         <ul className="nav-polls closed">
           <li className="shared">
             <Link to="poll" id="4">
-              Which movie is the greatest of all-time?
+              <span className="question">
+                Which movie is the greatest of all-time?
+              </span>
+              <span className="date">
+                5/8/2014
+              </span>
             </Link>
           </li>
           <li className="">
             <Link to="poll" id="5">
-              What are you going to get on the final
+              <span className="question">
+                What are you going to get on the final
+              </span>
+              <span className="date">
+                5/2/2014
+              </span>
             </Link>
           </li>
         </ul>
