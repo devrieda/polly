b6a3bbc2dcaf25416ed644089d39345c4c11f12b
Derek DeVries <devrieda@gmail.com>
2014-06-25 22:44:06 -0600

    get drawer navigation opening/closing when clicking menu

--- client/app/views/drawer_layout.js
+++ client/app/views/drawer_layout.js
@@ -6,14 +6,21 @@ var PollSubmissionForm = require('./poll_submission_form')
 var DrawerNav = require('./drawer_nav')
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return {'open': false}
+  },
+  handleMenuClick: function() {
+    this.setState({'open': !this.state.open});
+    return false;
+  },
   render: function() {
     return (
       <div className="drawer-layout">
-        <div className="drawer-content">
+        <div className={'drawer-content' + (this.state.open ? ' open' : '')}>
           <div className="wrap">
             <header className="title-bar">
               <div className="menu">
-                <a href="#">Menu</a>
+                <a href="#" onClick={this.handleMenuClick}>Menu</a>
               </div>
               <h1>Polling</h1>
             </header>
