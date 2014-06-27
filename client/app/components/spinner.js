f0c65ab782216f71288cd10a5d4b9d5e68c45563
Derek DeVries <devrieda@gmail.com>
2014-06-26 23:49:19 -0600

    clean up spinner

--- client/app/components/spinner.js
+++ client/app/components/spinner.js
@@ -2,23 +2,24 @@
 
 var React = require('react');
 
-module.exports = React.createClass({displayName: 'Spinner',
+module.exports = React.createClass({
   render: function() {
     var bars = [];
     var barStyle;
 
     for (var i = 0; i < 12; i++) {
       barStyle = {};
-      barStyle.WebkitAnimationDelay = barStyle.animationDelay = (i - 12) / 10 + 's';
+      barStyle.WebkitAnimationDelay = (i - 12) / 10 + 's';
+      barStyle.animationDelay = (i - 12) / 10 + 's';
       barStyle.WebkitTransform = barStyle.transform =
         'rotate(' + (i * 30) + 'deg) translate(9.5px)';
       bars.push(
-        React.DOM.div({style:barStyle, className:"spinner_bar"})
+        <div style={barStyle} className="spinner-bar" />
       );
     }
 
     return this.transferPropsTo(
-      React.DOM.div({className:"spinner"}, bars)
+      <div className="spinner">{bars}</div>
     )
   }
 });
